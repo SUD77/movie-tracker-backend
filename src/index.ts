3b5dcaa46345df2bf97f5ae82dec0eacfc3c73ae
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-10-25 02:01:35 +0530

    feat : server is running

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🎬 Movie Tracker Backend is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
